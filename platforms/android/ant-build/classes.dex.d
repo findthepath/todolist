e93513750ba62a071772619187c4a226267aa5d6
findthepath <saadmnabeel@gmail.com>
2014-11-23 17:25:05 +0800

    initiate commit

---
+++ platforms/android/ant-build/classes.dex.d
@@ -0,0 +1,9 @@
+C:\xampp\htdocs\com.revivalx.cordova.todomobile\platforms\android\ant-build\classes.dex : \
+C:\xampp\htdocs\com.revivalx.cordova.todomobile\platforms\android\ant-build\classes\io\cordova\hellocordova\BuildConfig.class \
+C:\xampp\htdocs\com.revivalx.cordova.todomobile\platforms\android\ant-build\classes\io\cordova\hellocordova\CordovaApp.class \
+C:\xampp\htdocs\com.revivalx.cordova.todomobile\platforms\android\ant-build\classes\io\cordova\hellocordova\R$attr.class \
+C:\xampp\htdocs\com.revivalx.cordova.todomobile\platforms\android\ant-build\classes\io\cordova\hellocordova\R$drawable.class \
+C:\xampp\htdocs\com.revivalx.cordova.todomobile\platforms\android\ant-build\classes\io\cordova\hellocordova\R$string.class \
+C:\xampp\htdocs\com.revivalx.cordova.todomobile\platforms\android\ant-build\classes\io\cordova\hellocordova\R$xml.class \
+C:\xampp\htdocs\com.revivalx.cordova.todomobile\platforms\android\ant-build\classes\io\cordova\hellocordova\R.class \
+C:\xampp\htdocs\com.revivalx.cordova.todomobile\platforms\android\ant-build\dexedLibs\classes-c220c3721e3441e4dde783256bd71e1f.jar \
